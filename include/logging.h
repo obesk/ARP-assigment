1c689f07000322566f569617ccccd223f9ea3d87
Roberto Bertelli <robbybertelli@gmail.com>
2025-02-07 23:24:14 +0100

    Improved logging functionality
    Signed-off-by: Roberto Bertelli <robbybertelli@gmail.com>

--- include/logging.h
+++ include/logging.h
@@ -6,16 +6,32 @@
 #include <time.h>
 
 // Log Levels
-#define LOG_INFO "INFO"
-#define LOG_DEBUG "DEBUG"
-#define LOG_WARN "WARN"
-#define LOG_ERROR "ERROR"
-#define LOG_CRITICAL "CRITICAL"
+enum LogLevels {
+	LOG_TRACE,
+	LOG_DEBUG,
+	LOG_INFO,
+	LOG_WARN,
+	LOG_ERROR,
+	LOG_CRITICAL,
+
+	LOG_N,
+};
+
+#define LOG_LEVEL LOG_DEBUG
+
+static const char *LEVEL_NAME[LOG_N] = {
+	[LOG_TRACE] = "TRACE", [LOG_DEBUG] = "DEBUG", [LOG_INFO] = "INFO",
+	[LOG_WARN] = "WARN",   [LOG_ERROR] = "ERROR", [LOG_CRITICAL] = "CRITICAL",
+};
 
 // Shared logging function
 // FIXME: seconds print
-void log_message(const char *level, const char *process, const char *format,
+void log_message(enum LogLevels level, const char *process, const char *format,
 				 ...) {
+	if (level < LOG_LEVEL) {
+		return;
+	}
+
 	FILE *logfile = fopen("simulation.log", "a");
 	if (!logfile) {
 		perror("Error opening log file");
@@ -29,7 +45,7 @@ void log_message(const char *level, const char *process, const char *format,
 
 	va_list args;
 	va_start(args, format);
-	fprintf(logfile, "[%s] [%s] [%s] ", timestamp, process, level);
+	fprintf(logfile, "[%s] [%s] [%s] ", timestamp, process, LEVEL_NAME[level]);
 	vfprintf(logfile, format, args);
 	fprintf(logfile, "\n");
 	va_end(args);
