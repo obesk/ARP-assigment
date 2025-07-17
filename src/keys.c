c01090dc99da9253c78f47108840650cdf43f4b7
Roberto Bertelli <robbybertelli@gmail.com>
2025-07-17 12:43:17 +0200

    Fixed keys arrays
    Signed-off-by: Roberto Bertelli <robbybertelli@gmail.com>

--- src/keys.c
+++ src/keys.c
@@ -1,11 +1,23 @@
 #include "keys.h"
+#include "logging.h"
+
+char KEYS[DIR_N] = {
+	[DIR_UP] = 'w',		   [DIR_UP_LEFT] = 'q',	 [DIR_LEFT] = 'a',
+	[DIR_DOWN_LEFT] = 'z', [DIR_DOWN] = 'x',	 [DIR_DOWN_RIGHT] = 'c',
+	[DIR_RIGHT] = 'd',	   [DIR_UP_RIGHT] = 'e', [DIR_STOP] = 's',
+};
+
+enum Direction DIRECTION_KEYS[CHARS_NUMBER] = { 0 };
 
 void keys_direction_init() {
+	log_message(LOG_INFO, "Initializing direction keys of size: %d", CHARS_NUMBER);
 	for (int i = 0; i < CHARS_NUMBER; ++i) {
 		DIRECTION_KEYS[i] = -1;
 	}
 
 	for (int i = 0; i < DIR_N; ++i) {
-		DIRECTION_KEYS[(int)KEYS[i]] = i;
+		const int index = (int)KEYS[i];
+		DIRECTION_KEYS[index] = i;
+		log_message(LOG_INFO, "index %d (char %c) = %d", index, index, i);
 	}
 }
