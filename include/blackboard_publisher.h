8b31ae822adb169e5bead2b2913dd6df0431925b
Roberto Bertelli <robbybertelli@gmail.com>
2025-07-18 09:57:32 +0200

    Copied sample publisher
    Signed-off-by: Roberto Bertelli <robbybertelli@gmail.com>

--- include/blackboard_publisher.h
+++ include/blackboard_publisher.h
@@ -1,6 +1,64 @@
 #ifndef BLACKBOARD_PUBLISHER_H
 #define BLACKBOARD_PUBLISHER_H
 
+#include "message.hpp"
+#include "messagePubSubTypes.hpp"
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/publisher/DataWriter.hpp>
+#include <fastdds/dds/publisher/DataWriterListener.hpp>
+#include <fastdds/dds/publisher/Publisher.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+
+#include <thread>
+
+//TODO: better not to use this
+using namespace eprosima::fastdds::dds;
+
+class BlackboardPublisher {
+ private:
+	Message message_;
+	DomainParticipant* participant_;
+	Publisher* publisher_;
+	Topic* topic_;
+	DataWriter* writer_;
+	TypeSupport type_;
+	// TODO: should probably implement outside
+	class PubListener : public DataWriterListener {
+	 public:
+		PubListener():
+			matched_(0) {
+		}
+		~PubListener() override { }
+		void on_publication_matched(DataWriter*, const PublicationMatchedStatus& info) override {
+			if (info.current_count_change == 1) {
+					matched_ = info.total_count;
+					std::cout << "Publisher matched." << std::endl;
+			}
+			else if (info.current_count_change == -1) {
+					matched_ = info.total_count;
+					std::cout << "Publisher unmatched." << std::endl;
+			} else {
+					std::cout << info.current_count_change
+							<< " is not a valid value for PublicationMatchedStatus current count change." << std::endl;
+			}
+		}
+		std::atomic_int matched_;
+	} listener_;
+ public:
+	BlackboardPublisher();
+	virtual ~BlackboardPublisher();
+
+	bool init();
+	//!Send a publication
+	bool publish();
+	//!Run the Publisher
+	void run( uint32_t samples);
+
+};
+
+
 extern "C" {
 
 int hello();
